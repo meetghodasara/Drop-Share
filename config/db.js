feecf0aad4f33c3a4ece83e65dcdf81a6238f785
Meet Ghodasara <meet.d.ghodasara@gmail.com>
2022-06-19 23:16:00 +0530

    Adding mongoDB Atlass

---
+++ config/db.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connectDB = () => {
+    mongoose.connect(process.env.MONGO_CONNECTION_URL , { useNewUrlParser:true ,useUnifiedTopology : true});
+    const connection = mongoose.connection;
+
+    connection.once('open' , () => {
+        console.log('Database connected');
+    })
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 3000;
 
+connectDB();
 app.listen( PORT , ()=>{
     console.log('Listning on port ' + PORT);
 })
